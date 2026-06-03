323026ce7fd695a16b320761126113400f33cff4
mr-SCWN <2004ivanko@gmail.com>
2026-06-03 17:39:38 +0200

    create table in SQL - Module 1

---
+++ sql/create_table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS machine_measurements ;
+
+CREATE TABLE machine_measurements (
+    udi                         INTEGER primary key,
+    product_id                  TEXT,
+    product_type                TEXT,
+    air_temperature_k           REAL,
+    process_temperature_k       REAL,
+    rotational_speed_rpm        INTEGER,
+    torque_nm                   REAL,
+    tool_wear_min               INTEGER,
+    machine_failure             INTEGER,
+    twf                         INTEGER,
+    hdf                         INTEGER,
+    pwf                         INTEGER,
+    osf                         INTEGER,
+    rnf                         INTEGER
+);
