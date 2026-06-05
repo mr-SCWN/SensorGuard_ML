6fec62b0336770d4e4ed7c736576bf19b4cd1510
mr-SCWN <2004ivanko@gmail.com>
2026-06-05 14:36:20 +0200

    Add feature engineering function - Module 2

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -15,12 +15,27 @@ def load_data_from_sql():
 
     # 4. Close connection
     connection.close()
-    
+
     # 5. Return DataFrame
     return df
 
+def add_features(df):
+    # Create a copy of the DataFrame
+    df_copy = df.copy()
+    # Add temperature difference feature
+    df_copy["temperature_difference"] = df_copy["process_temperature_k"] - df_copy["air_temperature_k"]
+    # Add power proxy feature
+    df_copy["power_proxy"] = df_copy["torque_nm"] * df_copy["rotational_speed_rpm"]
+    # Return updated DataFrame    
+    return df_copy
+
 
 if __name__ == "__main__":
-    df = load_data_from_sql()
+    df = load_data_from_sql() # verification load_data_from_sql
     print(df.head())
     print(df.shape)
+
+    df_with_features = add_features(df) # verification add_features 
+    print(df_with_features[['air_temperature_k', 'process_temperature_k', 'temperature_difference',
+            'torque_nm', 'rotational_speed_rpm', 'power_proxy']].head())
+    print(df_with_features.shape)
